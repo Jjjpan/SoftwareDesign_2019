292ac6ef73e3d5b485f58444a0cfb53ca822fa07
PanYJ <1025318325@qq.com>
2019-05-17 14:38:28 +0800

    Update an Quiz UI mode
    添加一个前端模型，但具体内容仍待商榷。

--- Software_Design_2019/client/utils/util.js
+++ Software_Design_2019/client/utils/util.js
@@ -1,42 +1,33 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
 
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
-const formatNumber = n => {
+function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
 
-
-// 显示繁忙提示
-var showBusy = text => wx.showToast({
-    title: text,
-    icon: 'loading',
-    duration: 10000
-})
-
-// 显示成功提示
-var showSuccess = text => wx.showToast({
-    title: text,
-    icon: 'success'
-})
-
-// 显示失败提示
-var showModel = (title, content) => {
-    wx.hideToast();
-
-    wx.showModal({
-        title,
-        content: JSON.stringify(content),
-        showCancel: false
-    })
+module.exports = {
+  formatTime: formatTime,
+  Ajax : function( data = '',method = "get",header = {},fn ){
+        wx.request({
+            url: config.API_HOST + data,
+            method : method ? method : 'get',
+            data: {},
+            header: header ? header : {"Content-Type":"application/json"},
+            success: function( res ) {
+                fn( res );
+            }
+        });
+    },
 }
 
-module.exports = { formatTime, showBusy, showSuccess, showModel }
